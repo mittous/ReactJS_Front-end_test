16ca5e98d4c10a696503385446b8924cb6ec0a46
root <root@mittous>
2024-06-03 02:47:01 +0100

    removing some tests leading a type errors

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,9 +2,8 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { NextResponse } from 'next/server';
 
-export async function GET(id :number) {
+export async function GET() {
   try {
-    console.log('Fetching products...', id);
     const dataFilePath = path.join(process.cwd(), 'src', 'database.json');
     const jsonData = await fs.readFile(dataFilePath, 'utf8');
     const data = JSON.parse(jsonData);
